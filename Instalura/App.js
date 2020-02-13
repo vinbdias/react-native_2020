17b6a805fb48b1b9e0c71f94f42dab1839107a83
USUARIO <usuario@mini-de-usuario.cbnet.com.br>
2020-02-13 14:47:41 -0200

    Primeiro commit.

---
+++ Instalura/App.js
@@ -0,0 +1,21 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Fragment} from 'react';
+import {Text} from 'react-native';
+
+const App = () => {
+  return (
+    <Fragment>
+      <Text>Vinícius</Text>
+      <Text>Luiz Filipe</Text>
+    </Fragment>    
+  );
+};
+
+export default App;
